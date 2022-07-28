7c6ca7316749de4e6841c0cf5d9162ad55fd8202
Ajey23 <ajaysarode2121@gmail.com>
2022-07-28 06:53:32 +0530

    new

---
+++ Core Java/MathFun.java
@@ -0,0 +1,12 @@
+//MATH FUNCTION IN JAVA
+
+class MathFun
+{
+    public static void main(String args[])
+    {
+        int n1=2, n2=3, n3=5;
+        System.out.println("\n Max is: "+ Math.max(n1,n2));
+        System.out.println("\n Power is: "+ Math.pow(n1,n2));
+        System.out.println("\n Squreroot is: "+ Math.sqrt(n3));
+    }
+}
